5cda7b8f7ed13036adc34387bb6d8b38cf1cea0f
Ygor Hernández <ygorhernandezbr@gmail.com>
2025-02-26 09:38:33 -0300

    Saving changes of day 1 and Creating fitle for day 2

--- Day1.js
+++ Day1.js
@@ -1,19 +1,37 @@
-let numeroUm = 1;
-let stringUm = '1';
-let numeroTrinta = 30;
-let stringTrinta = '30';
-let numeroDez = 10;
-let stringDez = '10';
+function comparar(a, b) {
+  let typeOfa = typeof (a);
+  let typeOfb = typeof (b);
 
+  if (a == b) {
+    let valor = "tem o mesmo valor";
+    if (typeOfa == typeOfb) {
+      console.log(`As variáveis ${a} e ${b} ${valor} e mesmo tipo`);
+    } else {
+      console.log(`As variáveis ${valor}, mas tipos diferentes`);
+    }
+  } else {
+    if (typeOfa == typeOfb) {
+      console.log(`As variáveis não tem o mesmo valor, mas tem o mesmo tipo`);
+    } else { 'As variáveis não tem o mesmo valor e nem o mesmo tipo' }
 
-function comparar(a,b){
-  let typeOfa = typeof(a);
-  let typeOfb = typeof(b);
-  if(a == b){
-    console.log(`As variáveis ${a} e ${b} tem o mesmo valor, mas tipos diferentes,o primeiro parametro ${a} é um ${typeOfa} e o segundo ${b} é um ${typeOfb}`)}
-  else {
-   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
   }
 }
 
-comparar(numeroUm,stringUm);
+/* if (COMPARAR O numeroUm e a stringUm) {
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+if (COMPARAR O numeroTrinta e a stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+if (COMPARAR O numeroDez e a stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+} 
+  */
