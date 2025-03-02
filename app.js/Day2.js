b040d7d85306919903e0c290bb4308308a70e5ec
Ygor Hernández <ygorhernandezbr@gmail.com>
2025-03-02 17:30:35 -0300

    Script Day 2

--- app.js/Day2.js
+++ app.js/Day2.js
@@ -1,58 +1,55 @@
 let content = document.getElementById('content');
+let copyBody = content.cloneNode(true);
+let contentMain = content;
 let negationButton = document.getElementById('negation');
 let sendButton = document.getElementById('send');
 let enquete = document.getElementById('enquete');
+let currentMessage = 1;
 
 enquete.addEventListener('submit', function (event) {
     // Prevenir la actualización de la página
     event.preventDefault();
+
     let userName = document.getElementById('userName').value;
     let age = document.getElementById('age').value;
     let learningCode = document.getElementById('learningCode').value;
     let message1 = `Olá ${userName}!, você tem ${age} anos e já está aprendendo ${learningCode}`;
-let message2 = `Você gosta de estudar ${learningCode}`;
-let message3 = `Muito bom! Continue estudando e você terá muito sucesso en ${learningCode}!`;
-let message4 = `Ahh que pena... Já tentou aprender outras linguagens?`;
-let currentMessage = 0;
-let contentMain = content;
+    let message2 = `Você gosta de estudar ${learningCode}`;
+    let message3 = `Muito bom! Continue estudando e você terá muito sucesso en ${learningCode}!`;
+    let message4 = `Ahh que pena... Mas pode aprender outras linguagens!`;
 
-    content.innerHTML = '<img src="/img/hi.webp" />' + message1;
+    contentMain.innerHTML = '<img src="/img/hi.webp" />' + message1;
     sendButton.type = 'button';
     sendButton.innerText = 'Continuar';
     negationButton.classList.remove('d-none');
 
+    sendButton.addEventListener('click', function () {
+        console.log(currentMessage);
+        if (currentMessage === 1) {
+            contentMain.innerHTML = message2;
+            sendButton.innerText = 'SI';
+            negationButton.innerText = 'NO';
+            console.log(currentMessage);
+        } else if (currentMessage === 2) {
+            contentMain.innerHTML = message3;
+            sendButton.classList.add('d-none');
+            negationButton.innerText = 'Salir';
+        }
+        currentMessage += 1;
+
+    });
+
+    negationButton.addEventListener('click', function () {
+        console.log(currentMessage);
+        if (currentMessage === 1 || currentMessage === 3 || currentMessage === 4) {
+            location.reload();
+        } else if (currentMessage === 2) {
+            contentMain.innerHTML = message4;
+            sendButton.classList.add('d-none');
+            negationButton.innerText = 'Salir';
+        }
+        currentMessage += 1;
+    });
+
 });
-
-
-function nextMessage(){
-if (currentMessage === 0){
-alert('Boton clicado SI');
-content.innerHTML = `Você gosta de estudar ${learningCode} ?`;
- sendButton.innerText = 'SI';
-    negationButton.innerText = 'NO';
-};
-
-
-sendButton.addEventListener('click', nextMessage);
-negationButton.addEventListener('click', nextMessage);
-
-/*
-sendButton.addEventListener('click', handleButtonClick);
-negationButton.addEventListener('click', handleButtonClick);
-let message2 = `Você gosta de estudar ${learningCode} ?\nResponda com o número 1 para SIM ou 2 para NÃO.?`;
-function handleButtonClick(event){
-    if (EventTarget === sendButton){
-        content.innerText =message2;
-    }
-
-}
-*/
-// let confirm = prompt(`Você gosta de estudar ${learningCode} ?\nResponda com o número 1 para SIM ou 2 para NÃO.?`);
-// if (confirm == 1) {
-//     alert(`Muito bom! Continue estudando e você terá muito sucesso en ${learningCode}!`);
-// } else {
-//     alert(`Ahh que pena... Já tentou aprender outras linguagens?`);
-// }
-
-
 
