981aa01f1b4464492b6016e899ab9e0f855577ec
ygor-github <ygorhernandezbr@gmail.com>
2025-03-03 01:42:14 -0300

    working on Day3

--- app.js/Day3.js
+++ app.js/Day3.js
@@ -1,5 +1,28 @@
 /* Script of Day 3 */
-let nickName = document.getElementById('nickName');
-let content = document.getElementById('content');
-console.log(nickName);
-console.log(content);
+
+let gameContent = document.getElementById('gameContent');
+let game = document.getElementById('game');
+let play = document.getElementById('play');
+game.addEventListener('submit', function (event) {
+	event.preventDefault();
+	let nickName = document.getElementById('nickName').value;
+	messageWelcome = `<h4>Olá ${nickName}!</h4>\n <p>Seja Bem-Vindo ao Jogo!</p>\n\n <div class="mb-3">
+  <label for="areaSelecionada" class="form-label">Você quer seguir para:</label>
+  <select class="form-select" id="areaSelecionada" name="areaSelecionada">
+    <option selected disabled>Selecione uma opção</option>
+    <option value="front-end">Área de Front-End</option>
+    <option value="back-end">Área de Back-End</option>
+  </select>
+</div>`;
+
+	gameContent.innerHTML = messageWelcome;
+	play.type = 'button';
+	play.innerText = 'Continuar';
+
+	let currentQuestion = 1;
+	play.addEventListener('click', function () {
+		if (currentQuestion === 1) {
+			alert('Pregunta 3');
+		}
+	})
+});
